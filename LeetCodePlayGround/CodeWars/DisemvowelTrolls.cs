d114a3d35a48e92719d1758f9e988f55c8203998
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-23 11:49:08 -0700

    another code wars problem solved + tests

---
+++ LeetCodePlayGround/CodeWars/DisemvowelTrolls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayGround.CodeWars
+{
+    public class DisemvowelTrolls
+    {
+        public static string Disemvowel(string message)
+        {
+            if (message == null) return string.Empty;
+            
+            StringBuilder sb = new StringBuilder();
+            List<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u','A','E','I','O','U' };
+            foreach(var c in message)
+            {
+                if (!vowels.Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodePlaygroundTests/CodeWars/DisemvowelTrollsTests.cs
@@ -0,0 +1,68 @@
+﻿using LeetCodePlayGround.CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlaygroundTests.CodeWars
+{
+    public class DisemvowelTrollsTests
+    {
+
+        [Fact]
+        public static void Disemvowel_RemovesAllVowelsFromString()
+        {
+            string message = "helloWorld";
+            string expected = "hllWrld";
+
+            string result = DisemvowelTrolls.Disemvowel(message);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void Disemvowel_RemovesAllVowelsFromString_2()
+        {
+            string message = "Looooool";
+            string expected = "Ll";
+
+            string result = DisemvowelTrolls.Disemvowel(message);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void Disemvowel_RemovesAllVowelsFromString_3()
+        {
+            string message = "No offense but,\nYour writing is among the worst I've ever read";
+            string expected = "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd";
+            
+            string result = DisemvowelTrolls.Disemvowel(message);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void Disemvowel_returnsEmptystringWhenInputMessageisEmpty()
+        {
+            string message = string.Empty;
+            string expected = string.Empty;
+
+            string result = DisemvowelTrolls.Disemvowel(message);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public static void Disemvowel_returnsEmptystringWhenInputMessageisNull()
+        {
+            string message = null;
+            string expected = string.Empty;
+
+            string result = DisemvowelTrolls.Disemvowel(message);
+
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
