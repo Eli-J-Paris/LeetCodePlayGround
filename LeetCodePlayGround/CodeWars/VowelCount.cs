7ba85f0232ba2ead1047d0c7524c94f05afd64b7
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-23 09:34:26 -0700

    VowelCountSolved+Tests

---
+++ LeetCodePlayGround/CodeWars/VowelCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayGround.CodeWars
+{
+    public class VowelCount
+    {
+        public static int GetVowelCount(string str)
+        {
+            List<char> vowels = new List<char> { 'a','e','i','o','u' };
+            int sum = 0;
+            foreach(char c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    sum++;
+                }
+            }
+            return sum;
+;        }
+    }
+}
---
+++ LeetCodePlaygroundTests/CodeWars/VowelCountTests.cs
@@ -0,0 +1,67 @@
+﻿using LeetCodePlayGround.CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlaygroundTests.CodeWars
+{
+    public class VowelCountTests
+    {
+        [Fact]
+        public static void GetVowelCount_ReturnsCountofVowels_InString()
+        {
+            string input = "aeiou";
+            int expected = 5;
+
+            int result = VowelCount.GetVowelCount(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void GetVowelCount_ReturnsCountofVowels_InString_2()
+        {
+            string input = "helloworld";
+            int expected = 3;
+
+            int result = VowelCount.GetVowelCount(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void GetVowelCount_ReturnsCountofVowels_InString_3()
+        {
+            string input = "ghtrwqvx";
+            int expected = 0;
+
+            int result = VowelCount.GetVowelCount(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void GetVowelCount_Returns0_IfStringIsEmpty()
+        {
+            string input = string.Empty;
+            int expected = 0;
+
+            int result = VowelCount.GetVowelCount(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void GetVowelCount_Returns0_IfString_isJustSpaces()
+        {
+            string input = "   ";
+            int expected = 0;
+
+            int result = VowelCount.GetVowelCount(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
