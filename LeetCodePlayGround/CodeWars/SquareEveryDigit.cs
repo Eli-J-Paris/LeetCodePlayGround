d3245b49c60b0352e91ec5dfecc950ac656dba66
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-24 10:46:33 -0700

    SquareEveryDigit+Tests

---
+++ LeetCodePlayGround/CodeWars/SquareEveryDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayGround.CodeWars
+{
+    public class SquareEveryDigit
+    {
+        public static int SquareDigits(int num)
+        {
+            string sqauredStr = string.Empty;
+            string strnum = num.ToString();
+            
+            foreach(var n in strnum)
+            {
+                int i = Convert.ToInt32(n.ToString());
+                sqauredStr += (i * i).ToString();
+            }
+
+            return Int32.Parse(sqauredStr);
+        }
+    }
+}
---
+++ LeetCodePlaygroundTests/CodeWars/SquareEveryDigitTests.cs
@@ -0,0 +1,45 @@
+﻿using LeetCodePlayGround.CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlaygroundTests.CodeWars
+{
+    public class SquareEveryDigitTests
+    {
+        [Fact]
+        public static void SquareDigits_SqauresEachnumberAndConcatsThemTogether()
+        {
+            int num = 55;
+            int expected = 2525;
+
+            int result = SquareEveryDigit.SquareDigits(num);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void SquareDigits_SqauresEachnumberAndConcatsThemTogether_2()
+        {
+            int num = 94;
+            int expected = 8116;
+
+            int result = SquareEveryDigit.SquareDigits(num);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void SquareDigits_SqauresEachnumberAndConcatsThemTogether_3()
+        {
+            int num = 1;
+            int expected = 1;
+
+            int result = SquareEveryDigit.SquareDigits(num);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
