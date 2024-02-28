743c34fe52f88ff153c367df5f73ec01a578f533
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-28 16:24:12 -0700

    GetMiddleCharacter + Tests

---
+++ LeetCodePlayGround/CodeWars/GetTheMiddleCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/56747fd5cb988479af000028/train/csharp
+namespace LeetCodePlayGround.CodeWars
+{
+    public class GetTheMiddleCharacter
+    {
+        public static string GetMiddle(string str)
+        {
+            string result = "";
+            int mid = str.Length / 2;
+            if (str.Length % 2 == 0)
+            {
+                result = str[mid -1] + str[mid].ToString();
+            }
+            else
+            {
+                result = str[mid].ToString();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodePlaygroundTests/CodeWars/GetTheMiddleCharacterTests.cs
@@ -0,0 +1,46 @@
+﻿using LeetCodePlayGround.CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlaygroundTests.CodeWars
+{
+    public class GetTheMiddleCharacterTests
+    {
+        [Fact]
+        public static void GetMiddle_ReturnsMiddleCharacterifstringLengthIsOdd()
+        {
+            string str = "abc";
+            string expected = "b";
+
+            string result = GetTheMiddleCharacter.GetMiddle(str);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void GetMiddle_ReturnsMiddleCharacterifstringLengthIsOdd_2()
+        {
+            string str = "abcdefg";
+            string expected = "d";
+
+            string result = GetTheMiddleCharacter.GetMiddle(str);
+
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public static void GetMiddle_ReturnsTwoMiddleCharactersifStringLengthIsEven()
+        {
+            string str = "abcd";
+            string expected = "bc";
+
+            string result = GetTheMiddleCharacter.GetMiddle(str);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
