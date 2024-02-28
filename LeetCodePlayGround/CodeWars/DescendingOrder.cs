49a05ff497a5d7bf1f4b769e77676843c63b8b1c
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-02-28 16:05:20 -0700

    DesendingOrder code wars problem + tests

---
+++ LeetCodePlayGround/CodeWars/DescendingOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayGround.CodeWars
+{
+    public class DescendingOrder
+    {
+        public static int OrderDescending(int num)
+        {
+            string strnum = num.ToString();
+            int[] numarry = num.ToString().Select(n => Convert.ToInt32(n)-48).ToArray();
+
+            Array.Sort(numarry);
+            Array.Reverse(numarry);
+            string result = String.Join("",numarry);
+           
+            return Convert.ToInt32(result);
+
+        }
+    }
+}
---
+++ LeetCodePlaygroundTests/CodeWars/DescendingOrderTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodePlayGround.CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlaygroundTests.CodeWars
+{
+    public class DescendingOrderTests
+    {
+        [Fact]
+        public static void OrderDescending_ReordersIntIntoDescendingOrder()
+        {
+            int num = 12345;
+            int expected = 54321;
+
+            int result = DescendingOrder.OrderDescending(num);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void OrderDescending_ReordersIntIntoDescendingOrder_2()
+        {
+            int num = 33617;
+            int expected = 76331;
+
+            int result = DescendingOrder.OrderDescending(num);
+
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
