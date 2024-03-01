d2f39f30bb3e890de5b6c269dd67480303ea6c39
Eli-J-Paris <Eli.J.Paris@gmail.com>
2024-03-01 07:05:45 -0700

    Middle Character and List Filtering

--- LeetCodePlayGround/CodeWars/GetTheMiddleCharacter.cs
+++ LeetCodePlayGround/CodeWars/GetTheMiddleCharacter.cs
@@ -22,6 +22,7 @@ namespace LeetCodePlayGround.CodeWars
             }
 
             return result;
+
         }
     }
 }
---
+++ LeetCodePlayGround/CodeWars/ListFiltering.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayGround.CodeWars
+{
+    public class ListFiltering
+    {
+
+        public static IEnumerable<int> GetIntergersFromList(List<object> listOfItems)
+        {
+            List<int> trueInts = new List<int>();
+
+            foreach(var element in listOfItems)
+            {
+                if(element is int)
+                {
+                    trueInts.Add((int)element);
+                }
+            }
+            return trueInts;
+        }
+    }
+}
